f529f6c7c133038bacf9293201730b8e69c840df
FlyingDragon <793407247@qq.com>
2020-11-14 19:15:48 +0800

    生成profile

--- src/init_profile.sh
+++ src/init_profile.sh
@@ -1,7 +1,16 @@
-profile_path="${base_dir}profile.cfg"
+profile_path="${base_dir}.profile.cfg"
 
 create_template_profile() {
-  echo "v2ray_transport=1" >> "${profile_path}"
+  {
+    echo "#重要的事情说三遍! 千万不要修改或者删除这个文件! 我都隐藏了你还能找到!" #1s
+    echo "#重要的事情说三遍! 千万不要修改或者删除这个文件! 我都隐藏了你还能找到!" #2s
+    echo "#重要的事情说三遍! 千万不要修改或者删除这个文件! 我都隐藏了你还能找到!" #3s
+    echo ""                                                                                                       #4s
+    echo "#传输协议"                                                                                          #5s
+    echo "v2ray_transport=1"                                                                                      #6s
+    echo "#端口号"
+    echo "v2ray_port=443" #7s
+  } >>"${profile_path}"
 }
 
 backup_all_cfg() {
